d1ffb77045db6c57fd9d18d0cd575f4c53bce433
Sofian Antri <santri@emi.u-bordeaux1.fr>
2020-02-13 17:24:00 +0100

    copy et padding ok

--- im-proc/zoom/copy.c
+++ im-proc/zoom/copy.c
@@ -1,12 +1,11 @@
-#include <float.h>
 #include <stdlib.h>
-#include <math.h>
+#include <stdio.h>
 #include <bcl.h>
 
 #define NB_PARAMS 3
 
 /**
- * @brief Puts the same pixel depending on the factor.
+ * @brief Copy the same pixel depending on the factor.
  * @param ims the image to get the pixel
  * @param imd the image to write
  * @param col the index of the column write
@@ -14,10 +13,16 @@
  * @param factor the zoom factor
  */
 void
-put_pixels(pnm ims, pnm imd, int col, int row, int factor)
+copy_pixels(pnm ims, pnm imd, int col, int row, int factor)
 {
-    //TODO
-}
+    unsigned short c[3];
+    for(int k = 0; k < 3; k++) 
+        c[k] = pnm_get_component(ims, col, row, k);
+    for(int i = 0; i < factor; i++)
+        for(int j = 0; j < factor; j++)
+            for(int k=0; k < 3; k++){
+                pnm_set_component(imd, factor*col+j, factor*row+i, k, c[k]);
+}}
 
 void
 usage(const char *s)
@@ -27,28 +32,26 @@ usage(const char *s)
 }
 
 int 
-main(int argc, char* argv){
-
+main(int argc, char** argv)
+{
     if(argc != NB_PARAMS+1)
         usage(argv[0]);
     
     pnm ims = pnm_load(argv[2]);
     int cols = pnm_get_width(ims);
-    int rows = pnm_get_height(img);
+    int rows = pnm_get_height(ims);
     int factor = atoi(argv[1]);
 
     pnm imd = pnm_new(cols*factor, rows*factor, PnmRawPpm);
-
-    for(int i = 0; i < rows, i++){
-        for(int j = 0; j < cols; i++){
-            put_pixels(ims, imd, j, i, factor);
-        }
-    }
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < cols; j++){
+            copy_pixels(ims, imd, j, i, factor);
+        } 
 
     pnm_save(imd, PnmRawPpm, argv[3]);
 
-    pnm_free(imd)
-    pnm_free(ims)
+    pnm_free(imd);
+    pnm_free(ims);
 
     return EXIT_SUCCESS;
 }
